ffbdb718ad19a40362ee832f0d9dd1b55c78f685
Roman <rgeluz22@yahoo.com>
2020-09-22 15:19:51 -0700

    used io.Copy, this is a function that takes two arguments that implment Writer and Reader interfaces

--- interface_http/main.go
+++ interface_http/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
 func main() {
+	resp, err := http.Get("https://www.google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	//fmt.Println(resp)
+
+	/*
+		//bs := []byte{}
+		bs := make([]byte, 99999) //takes a slice and adds number of elements indicated exp 99999 elements. We choose 99999 arbitrarily, assuming this is large enough capacity
+
+		resp.Body.Read(bs) //read is adding data into the bs "byteslice"
+		fmt.Println(string(bs))
+	*/
+
+	//shorter version
+	io.Copy(os.Stdout, resp.Body)
 
 }
