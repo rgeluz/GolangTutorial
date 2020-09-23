72c44ce7a6846dd4e1dfe903e3886e8657d9a80d
Roman <rgeluz22@yahoo.com>
2020-09-22 18:51:58 -0700

    added a channel to the checkLink() function

--- channels/main.go
+++ channels/main.go
@@ -14,13 +14,27 @@ func main() {
 		"http://amazon.com",
 	}
 
-	for _, link := range links {
+	//This calls checkLink in a serial/sequential manner
+	/*for _, link := range links {
 		checkLink(link)
+	} */
+
+	//added go keyword. When running this, the main function ended before finishing each of the child go routines. Need to use channels
+	/*for _, link := range links {
+		go checkLink(link)
+	}*/
+
+	//need to pass channel to checklink
+	c := make(chan string)
+	for _, link := range links {
+		go checkLink(link, c)
 	}
 
+	fmt.Println(<-c) //receive a value from this channel and then log it
 }
 
-func checkLink(link string) {
+//this function will be used as a goroutine
+/* func checkLink(link string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
@@ -28,4 +42,17 @@ func checkLink(link string) {
 	}
 
 	fmt.Println(link, "is up!")
+} */
+
+//this function uses a channel
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	c <- "Yep its up"
 }
